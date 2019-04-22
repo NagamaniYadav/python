4919ee641bb379866706b3085fc2c164a2a6030c
NagamaniYadav <49432875+NagamaniYadav@users.noreply.github.com>
2019-04-22 10:47:01 +0530

    Add files via upload

---
+++ ver3gmail.py
@@ -0,0 +1,21 @@
+import smtplib
+import pymysql as dbms
+
+
+c=db.cursor()
+c.execute('select Mail,Name from employee')
+email=list(c.fetchall())
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from pathlib import Path
+from email.utils import COMMASPACE, formatdate
+
+connection=smtplib.SMTP('smtp.gmail.com',587)
+connection.ehlo()
+connection.starttls()
+print("started")
+
+connection.quit()
